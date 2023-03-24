8d9a888ce06a7a26307cbcde367b49b1807b429c
gezish <enggezahegn.w@gmail.com>
2023-03-24 21:10:58 +0300

    dictionery done

--- app.py
+++ app.py
@@ -1,22 +1,53 @@
-from flask import Flask, jsonify, request
+# dictionary-api-python-flask/app.py
+from flask import Flask, request, jsonify, render_template
+from model.dbHandler import match_exact, match_like
 
-# Intitialise the app
 app = Flask(__name__)
-# Define what the app does
-@app.get("/greet")
+
+
+@app.get("/")
 def index():
-    fname = request.args.get("fname")
-    lname = request.args.get("lname")
+    """
+    DEFAULT ROUTE
+    This method will
+    1. Provide usage instructions formatted as JSON
+    """
+    #response = {"usage": "/dict?=<word>"}
+    # Since this is a website with front-end, we don't need to send the usage instructions
+    return render_template("index.html")
 
-    if not fname and not lname:
-        return jsonify({"status" : "error"})
-    elif fname and not lname:
-        response = {"data" : f"Hello, {fname} !"}
-    elif not fname and lname:
-        response = {"data" : f"Hello, Mr. {lname} ?"}
-    else:
-        response = {"data" : f"Is your name , {fname} {lname} ?"}
-    
-    return jsonify(response)
 
-    
+@app.get("/dict")
+
+
+def dictionary():
+    """
+    DEFAULT ROUTE
+    This method will
+    1. Accept a word from the request
+    2. Try to find an exact match, and return it if found
+    3. If not found, find all approximate matches and return
+    """
+    words = request.args.getlist("word")
+    if not words:
+        response = {"status": "error", "word": "words","data": "Word not Found"}
+        return jsonify(response)
+
+    response = {"words":[]}   
+    for word in words:
+
+        definitions = match_exact(word)
+
+        if definitions:
+            response["words"].append({"status": "success", "data": definitions,"words":word})
+        else:
+            definitions = match_like(word)
+            if definitions:
+                response["words"].append({"status": "partial", "data": definitions, "words":word})
+            else:
+                response["words"].append({"status": "error", "data":"word not found","word": "words"})
+    return render_template("results.html", response = jsonify(response))
+
+
+if __name__ == "__main__":
+    app.run()
